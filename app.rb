be687f5f68a9e252ab0e117512323503ff19c2b3
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-06-06 10:13:24 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Garren"
+  end
+
+  get '/hometown' do
+    "My hometown is Hudson, OH"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is - N/A - to many to count"
+  end
 end
